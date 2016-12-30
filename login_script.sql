4eb016a0f39245bb119b3ce5b49cf6952d274767
sleepingsociety <sleepingsociety@users.noreply.github.com>
2016-12-31 00:11:04 +0100

    Delete login_script.sql

--- login_script.sql
+++
@@ -1,8 +0,0 @@
-DROP TABLE IF EXISTS users;
-CREATE TABLE users (
-  id int(11) NOT NULL AUTO_INCREMENT,
-  username text NOT NULL,
-  password text NOT NULL,
-  psalt text NOT NULL,
-  PRIMARY KEY (`id`)
-);
