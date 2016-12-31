5014c84e8d192fc6c2f06b17fd702e9117361502
sleepingsociety <chef007david@gmail.com>
2016-12-31 01:30:46 +0100

    Added Session protection

--- includes/welcomeMessage.php
+++ includes/welcomeMessage.php
@@ -1,5 +1,26 @@
+<?php
+$dbname    = $_SERVER['DB_NAME'];
+$servername    = $_SERVER['DB_HOST'];
+$dbusername    = $_SERVER['DB_USERNAME'];
+$dbpassword = $_SERVER['DB_PASSWORD'];
+
+$connection = new mysqli($servername, $dbusername, $dbpassword);
+
+if ($connection -> connect_error) {
+    die("Connection failed: " . $connection -> connect_error);
+}
+
+
+if(!isset($_SESSION)){
+    session_start();
+}
+
+if (!$_SESSION["login"]) header('Location: /index.php');
+
+?>
+
 <div id="welcomeMessage">
-    <h3>Willkommen zurück "Benutzername"</h3>
+    <h3>Willkommen zurück <?php echo $_SESSION['username']; ?></h3>
     <p>Von hier aus können Sie auf die verschiedenen Bereiche<br>
         dieser Webseite zugreifen.</p>
     <p>"Platz für eine extra Nachricht oder so"</p>
